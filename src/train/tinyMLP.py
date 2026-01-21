a07db449baee9fde30117c92485af66bbd573588
GalaxAI <92019780+GalaxAI@users.noreply.github.com>
2026-01-21 17:28:45 +0100

    working EPOCH

---
+++ src/train/tinyMLP.py
@@ -0,0 +1,53 @@
+from tinygrad import nn
+from tinygrad.engine.jit import TinyJit
+from tinygrad.helpers import trange
+from tinygrad.nn import optim, state
+from tinygrad.tensor import Tensor
+
+from src.train.load import ParquetDataLoader
+
+
+class TinyMLP:
+    def __init__(self):
+        self.l1 = nn.Linear(2048, 1024)
+        self.l2 = nn.Linear(1024, 512)
+        self.l3 = nn.Linear(512, 256)
+        self.l4 = nn.Linear(256, 1)
+
+    def __call__(self, x):
+        x = self.l1(x).relu()
+        x = self.l2(x).relu()
+        x = self.l3(x).relu()
+        x = self.l4(x)
+        return x
+
+
+EPOCHS = 1
+LR = 0.01
+BATCH_SIZE = 2028
+if __name__ == "__main__":
+    model = TinyMLP()
+    dataloader = ParquetDataLoader(
+        dir="data/chembl_36/fp_train", X="morgan_fp", Y="pIC", batch_size=BATCH_SIZE
+    )
+    optimizer = optim.Muon(state.get_parameters(model), lr=LR)
+
+    @TinyJit
+    @Tensor.train()
+    def train_step(x: Tensor, y: Tensor) -> Tensor:
+        optimizer.zero_grad()
+        preds = model(x)
+        loss = (preds - y).square().mean()
+        loss.backward()
+        optimizer.step()
+        return loss.realize()
+
+    print("Training started")
+    for epoch in range(EPOCHS):
+        loader_iter = iter(dataloader)
+        for step in (t := trange(len(dataloader))):
+            x, y = next(loader_iter)
+            print(x.shape)
+            loss = train_step(x, y)
+            if step % 10 == 9:
+                t.set_description(f"loss: {loss.item():6.2f}")
